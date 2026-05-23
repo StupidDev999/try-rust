6bd9b07f3078f1e698c644ab88511d97cdb933a3
StupidDev999 <127037168+StupidDev999@users.noreply.github.com>
2026-05-23 10:02:07 +0000

    Got arg parse but rust nags like a bitch
    Primitive arg parsing
    But why the fuck is a compiler
    telling me how to name my vars?
    Can't it just not be a pricky
    dickhead and just produce the
    binary?
    
    Anyways works...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 use extabs::Expand;
+use std::env;
 
 fn main() {
     let s = String::from("Hewwo\tWorld!");
     let expanded = s.expandtabs(4);
 
-    println!("{}", expanded);
+    let args: Vec<String> = env::args().collect();
+
+    let mut errorArgs = false;
+    for arg in &args {
+        if arg == "-h" || arg == "--help" {
+            println!("Help prompt!");
+            return;
+        }
+        else if arg == "-C" || arg == "--disable-colour" {
+            println!("Disabled Colours!");
+        }
+        else if arg == "-w" || arg == "--enable-warn" {
+            println!("Enabled Warning");
+        }
+        else if arg == "-v0" {
+            println!("Verbosity 0");
+        }
+        else if arg == "-v1" {
+            println!("Verbosity 1");
+        }
+        else if arg == "-v2" {
+            println!("Verbosity 2");
+        }
+        else if arg == "-v3" {
+            println!("Verbosity 3");
+        }
+        else {
+            errorArgs = true;
+        }
+    }
+
+    if errorArgs {
+        println!("Unknown/Erroneous arguments found, ignored them.");
+    }
+
+    println!("{}", expanded)
 }
